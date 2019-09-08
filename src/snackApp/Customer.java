d78ce80ad40ca7ca02120797c1304f7cd87fa72f
jeffyjkang <jeffyjkang@gmail.com>
2019-09-07 21:10:30 -0700

    initial attemp at setup for snack app

---
+++ src/snackApp/Customer.java
@@ -0,0 +1,40 @@
+package snackApp;
+
+public class Customer {
+    private static int maxId = 0;
+    private int id;
+    private String name;
+    private double cashOnHand;
+
+    public Customer(String name, double cashOnHand) {
+        maxId++;
+        id = maxId;
+        this.name = name;
+        this.cashOnHand = cashOnHang;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addCash(double cash) {
+        this.cashOnHand = this.cashOnHand + cash;
+    }
+
+    public void buySnack(double cash) {
+        this.cashOnHand = this.cashOnHand - cash;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCashOnHand() {
+        return cashOnHand;
+    }
+
+}
